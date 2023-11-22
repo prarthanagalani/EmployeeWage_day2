b689ff44475c38dfbcf619ff0fc75ff07863ceec
prarthanagalani <prarthanagalani@gmail.com>
2023-11-22 10:57:49 +0530

    added: employee attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,26 @@
 package EmployeeWage_day2;
 
-
+import java.util.Random;
 
 public class EmployeeWage{
 
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOURS = 8;
+    private static final int PART_TIME_HOURS = 4;
+    private static final int WORKING_DAYS_PER_MONTH = 20;
+    private static final int MAX_WORKING_HOURS = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int attendance = checkEmployeeAttendance();
+        System.out.println("Employee is " + (attendance == 1 ? "Present" : "Absent"));
+    }
+
+    private static int checkEmployeeAttendance() {
+        Random random = new Random();
+        int attendance = random.nextInt(2); // 0 for absent, 1 for present
+        return attendance;
     }
 
 }
